863d91412ab8bea07a3a5c3b1063b82b10024694
DawidMk <dawid_m@interia.pl>
2018-12-16 11:56:53 +0100

    random

---
+++ src/main/java/pl/sda/javastart/day4/RandomGen.java
@@ -0,0 +1,24 @@
+package pl.sda.javastart.day4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomGen {
+
+    public static int[] createRandomArray(int howLong) {
+        int[] tab = new int[howLong];
+
+        Random random = new Random();
+
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = random.nextInt();
+        }
+        return tab;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(createRandomArray(5)));
+
+
+    }
+}
