c0c9ef3480dcdea8ba70642e61ceae9ebec7af97
DawidMk <dawid_m@interia.pl>
2018-12-28 18:36:37 +0100

    day2_homeTabs_loopInloop_added

--- src/main/java/pl/sda/javastart/day4/homework/Homework27.java
+++ src/main/java/pl/sda/javastart/day4/homework/Homework27.java
@@ -1,6 +1,7 @@
 package pl.sda.javastart.day4.homework;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Homework27 {
 
@@ -21,26 +22,26 @@ public class Homework27 {
             sum[firstTab.length + i] = secondTab[i];
         }
         Arrays.sort(sum);
-       int[] result = removeDuplicates(sum);
+        int[] result = removeDupesLoopInLoop(sum);
         return result;
 
     }
 
     public static int[] removeDuplicates(int[] tab) {
-        String output = tab[0]+",";
+        String output = tab[0] + ",";
 
         for (int i = 0; i < tab.length; i++) {
             if (i == 0) {
                 continue;
             }
             if (tab[i] != tab[i - 1]) {
-            output = output+tab[i]+",";
+                output = output + tab[i] + ",";
             }
         }
         String[] items = output.split(",");
         int[] result = new int[items.length];
-        for (int i = 0; i < items.length ; i++) {
-            result[i]=Integer.parseInt(items[i]);
+        for (int i = 0; i < items.length; i++) {
+            result[i] = Integer.parseInt(items[i]);
 
         }
         return result;
@@ -73,12 +74,48 @@ public class Homework27 {
     }
      */
 
+    private static int[] removeDupesLoopInLoop(int[] tab) {
+
+        int[] temp = new int[]{tab[0]};
+        for (int i : temp) {
+            for (int j : tab) {
+                if (j != i) {
+                    temp = Arrays.copyOf(temp, temp.length + 1);
+                    temp[i + 1] = j;
+                    i++;
+                }
+            }
+        }
+        return temp;
+    }
+
+    //    IntStream.of(tab).anyMatch(x -> x == input))
+ /*   private static void removeDupesStream(int[] tab) {
+        int[] temp = new int[]{tab[0]};
+        for(int i : temp){
+            for(int j : tab){
+                if(IntStream.of(temp).anyMatch(x -> x != j)){
+                    temp = Arrays.copyOf(temp, temp.length + 1);
+                    temp[i]=j;
+                    i;
+
+                }
+            }
+        System.out.println(Arrays.toString(temp));
+        }}*/
+
+
+
+
     public static void main(String[] args) {
-        int[] tabFirst = {0,3,4,6,2,1};
-        int[] tabSec = {5,6,9,7,0,20,3};
-        int[] tabThird = concatenateAndSortArray(tabFirst,tabSec);
-//        System.out.println(Arrays.toString(concatenateAndSortArray(tabFirst,tabSec)));
-        System.out.println(Arrays.toString(removeDuplicates(tabThird)));
+        int[] tabFirst = {1, 0, 3, 4, 6, 2, 1};
+        int[] tabSec = {5, 6, 9, 7, 0, 20, 3};
+        int[] tabThird = concatenateAndSortArray(tabFirst, tabSec);
+        int[] tabFourth = concatenateArrays(tabFirst, tabSec);
+        Arrays.sort(tabFourth);
+//        System.out.println(Arrays.toString(concatenateAndSortArray(tabFirst, tabSec)));
+//        System.out.println(Arrays.toString(removeDuplicates(tabThird)));
+        removeDupesStream(tabFourth);
 
 
     }
